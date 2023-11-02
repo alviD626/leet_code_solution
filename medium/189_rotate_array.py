8e0439f6e7faa14e5defaca2cb9e9b3712c35fe4
alvi <alimostakim68@gmail.com>
2023-11-02 21:56:48 +0600

    add 2nd november problem

--- medium/189_rotate_array.py
+++ medium/189_rotate_array.py
@@ -6,10 +6,12 @@ class Solution(object):
         :type k: int
         :rtype: None Do not return anything, modify nums in-place instead.
         """
+        # Solution 1
         for i in range(k):
             previous = nums[-1]
             for j in range(len(nums)):
-                temp = nums[i]
-                nums[i] = previous
-                previous = temp
-        logging.debug(f"nums: {nums}")
+                nums[j], previous= previous, nums[j]
+                
+        # Solution 2
+        k=k%len(nums)
+        nums[:]=nums[-k:]+nums[:-k]
