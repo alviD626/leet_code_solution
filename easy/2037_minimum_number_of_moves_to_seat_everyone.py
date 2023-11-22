1022d47cf9906e512171f206e0c9cf8898438e4e
alvi <alimostakim68@gmail.com>
2023-11-22 08:46:01 +0600

    add new easy problem

---
+++ easy/2037_minimum_number_of_moves_to_seat_everyone.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def minMovesToSeat(self, seats, students):
+        """
+        :type seats: List[int]
+        :type students: List[int]
+        :rtype: int
+        """
+        seats.sort()
+        students.sort()
+        return sum(abs(seat - student) for seat, student in zip(seats, students))
+
