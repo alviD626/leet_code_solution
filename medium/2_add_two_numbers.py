57e3d8f3edfe00a8eabb0d75c38893935c1b3f43
alvi <alimostakim68@gmail.com>
2023-11-25 07:33:57 +0600

    add a medium level problem but it's incomplete

---
+++ medium/2_add_two_numbers.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        l1.reverse()
+        l2.reverse()
+        l3 = ''.join(map(str,l1))
+        l4 = ''.join(map(str,l2))
+        l5 = int(l3)
+        l6 = int(l4)
+        return l5 + l6
