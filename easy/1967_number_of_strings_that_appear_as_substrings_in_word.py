86c2b668083c8ff56de566faa7ee44d65e51152b
alvi <alimostakim68@gmail.com>
2023-11-25 10:09:15 +0600

    add new easy problem

---
+++ easy/1967_number_of_strings_that_appear_as_substrings_in_word.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def numOfStrings(self, patterns, word):
+        """
+        :type patterns: List[str]
+        :type word: str
+        :rtype: int
+        """
+        return len([p for p in patterns if p in word])
