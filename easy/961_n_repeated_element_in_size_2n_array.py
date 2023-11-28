e19a24992630f07c33525753a682747e633b9eae
alvi <alimostakim68@gmail.com>
2023-11-28 13:54:03 +0600

    add new easy solution

---
+++ easy/961_n_repeated_element_in_size_2n_array.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def repeatedNTimes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        list1 = []
+        for i in nums:
+            if i in list1:
+                return i
+            else:
+                list1.append(i)
+        return list1
